0b45a6f97a1edf4532e5a1c60d53cb8ec3dc31c8
Isaac Amin Sofan Hernandez <digimonataque@gmail.com>
2025-05-02 14:28:05 -0500

    optimized toggle theme with typeScript and service

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ThemeService } from '../../services/theme.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  private tema = inject(ThemeService);
+  currentTheme: string = 'auto';
 
+  ngOnInit(): void{
+    this.currentTheme = this.tema.getCurrentTheme();
+  }
+
+  toggleTheme(theme: 'light' | 'dark' | 'auto'){
+    this.tema.setTheme(theme);
+    this.currentTheme = theme;
+  }
 }
---
+++ src/app/services/theme.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeService {
+  private html = document.documentElement;
+
+  constructor() {
+    this.initTheme();
+  }
+
+  private initTheme(): void {
+    const theme = localStorage.getItem('hs_theme') || 'auto';
+    const preferDark = window.matchMedia(
+      '(prefers-color-scheme: dark)'
+    ).matches;
+    const isLightOrAuto =
+      theme === 'light' || (theme === 'auto' && !preferDark);
+    const isDarkOrAuto = theme === 'dark' || (theme === 'auto' && preferDark);
+    if (isLightOrAuto && this.html.classList.contains('dark')) {
+      this.html.classList.remove('dark');
+    } else if (isDarkOrAuto && !this.html.classList.contains('dark')) {
+      this.html.classList.add('dark');
+    }
+  }
+
+  setTheme(theme: 'light' | 'dark' | 'auto') {
+    localStorage.setItem('hs_theme', theme);
+    this.initTheme(); // Reapply logic
+  }
+
+  getCurrentTheme(): string{
+    return localStorage.getItem('hs_theme') || 'auto';
+  }
+}
