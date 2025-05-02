79de842b55ebebd43f9e161401f318a7d3613e87
Isaac Amin Sofan Hernandez <digimonataque@gmail.com>
2025-05-02 15:38:28 -0500

    optimized header with collapse menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import { ThemeService } from '../../services/theme.service';
 import { CommonModule } from '@angular/common';
 
@@ -11,6 +11,8 @@ import { CommonModule } from '@angular/common';
 export class HeaderComponent {
   private tema = inject(ThemeService);
   currentTheme: string = 'auto';
+  activeSection: string = '';
+  isMenuOpen: boolean = false;
 
   ngOnInit(): void{
     this.currentTheme = this.tema.getCurrentTheme();
@@ -19,5 +21,29 @@ export class HeaderComponent {
   toggleTheme(theme: 'light' | 'dark' | 'auto'){
     this.tema.setTheme(theme);
     this.currentTheme = theme;
+  }
+
+  setActive(section: string) {
+    this.activeSection = section;
+  }
+
+  toggleMenu(){
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  selectMenuItem(section: string){
+    this.setActive(section);
+    this.isMenuOpen = false; //close menu at select item
+  }
+
+  @HostListener('document: click', ['$event']) onClick(event: MouseEvent){
+    const target = event.target as HTMLElement;
+    const menu = document.getElementById('hs-navbar-header-floating');
+    const toggleButton = document.getElementById('hs-navbar-header-floating-collapse');
+
+    // close menu click item or outside menu
+    if (menu && !menu.contains(target) && toggleButton && !toggleButton.contains(target)) {
+      this.isMenuOpen = false;
+    }
   }
 }
